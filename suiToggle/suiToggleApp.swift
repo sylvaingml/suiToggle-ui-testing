396c632913dcf5d65c08166c481ce0b531c62170
Sylvain Gamel <dev@sylvaingamel.fr>
2023-02-09 08:26:12 +0100

    Initial Commit

---
+++ suiToggle/suiToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  suiToggleApp.swift
+//  suiToggle
+//
+//  Created by Sylvain on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct suiToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
